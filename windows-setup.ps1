89c4a3d8efc6f6cb3a537c5d673593efca9b0145
Daron <mcintoshdaron@gmail.com>
2023-01-12 08:15:50 -0800

    changes

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,38 +1,19 @@
-# https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages
-# install winget if not installed (should always be installed but for sandbox it isn't)
+# winget comes bundled in modern versions of windows: https://learn.microsoft.com/en-us/windows/package-manager/winget/
 
-Invoke-WebRequest -URI "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile vcl.appx
-Add-AppxPackage vcl.appx
+# create powershell profile if not exists: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.3
+if (!(Test-Path -Path $PROFILE)) {
+  New-Item -ItemType File -Path $PROFILE -Force
+}
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-Invoke-WebRequest -URI "https://github.com/microsoft/winget-cli/releases/download/v1.4.3531/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile winget.msixbundle
-Add-AppxPackage winget.msixbundle
+# rancher desktop is a bit buggy at the moment so we'll stick with docker desktop for now
+winget import --import-file winstall.json
 
-
-# rancher for windows and docker for linux
-# maybe not rancher yet cause it's a bit buggy
-# winget install -e --id suse.RancherDesktop
-# winget install -e --id Docker.DockerDesktop
-# winget install -e --id Microsoft.VisualStudioCode
-# winget install -e --id Google.Chrome
-# winget install -e --id Microsoft.WindowsTerminal
-
-# https://learn.microsoft.com/en-us/windows/terminal/tutorials/custom-prompt-setup
-winget install XP8K0HKJFRXGCK
 # install font
 Invoke-WebRequest -URI "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip" -Out FiraCode.zip
-Invoke-WebRequest -URI "https://github.com/microsoft/terminal/releases/download/v1.15.3465.0/Microsoft.WindowsTerminal_Win10_1.15.3465.0_8wekyb3d8bbwe.msixbundle" -Out terminal.msixbundle
-Add-AppxPackage terminal.msixbundle
 # extract zip to C:\Windows\fonts
 
-# source control terminal config file too
-
-Set-ExecutionPolicy RemoteSigned
-mkdir -p C:\Users\WDAGUtilityAccount\Documents\WindowsPowerShell
-notepad $PROFILE
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
-Set-ExecutionPolicy Restricted
 # install wsl
 # install ubuntu from a tar
 # run setup linux-setup.sh in container
 wsl --install
-
